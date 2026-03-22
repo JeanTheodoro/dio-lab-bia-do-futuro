1ef25b5abd426eb3b14366fe0ba18d0eb5a43151
Jean Theodoro <jean14silva2009@gmail.com>
2026-03-22 12:47:53 -0300

    feat: Craeted factory

--- src/api/llm/factory/factory_llm.py
+++ src/api/llm/factory/factory_llm.py
@@ -1,8 +1,6 @@
 # src/api/llm/factory/factory_llm.py
 from core.shared.settings import settings
 from llm.provider.ollma_provider import OllamaProvider
-from llm.provider.grok_provider import GrokProvider
-
 
 class LLMFactory:
 
@@ -11,8 +9,5 @@ class LLMFactory:
 
         if settings.LLM_PROVIDER == "ollama":
             return OllamaProvider()
-
-        elif settings.LLM_PROVIDER == "grok":
-            return GrokProvider()
 
         raise ValueError("LLM_PROVIDER inválido")
